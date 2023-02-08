998d4e47ae180fdbd0d75d377416d70cc721a86d
bearom95 <bearom95@gmail.com>
2023-02-08 21:29:43 +0100

    3 paginas a medias

--- src/pages/Despacho.jsx
+++ src/pages/Despacho.jsx
@@ -1,3 +1,30 @@
+import despacho from '../../public/despachoAbogados.jpg';
+import { Img } from '../components/Img';
+import { StyledDiv } from '../components/StyledDiv';
+
 export const Despacho = () => {
-  return <h1>Despacho</h1>;
+  return (
+    <StyledDiv display="flex" flexdir="column" justify="center" padding="2rem 5rem">
+      <StyledDiv display="flex" margin="1rem 0" gap="1rem" flexdir="column">
+        <Img src={despacho} alt="despacho" width="50%" />
+        <StyledDiv>
+          <p>
+            En Ódena podremos darte la ayuda personalizada en distintos ámbitos del
+            derecho, con el fin de darte el mejor resultado.
+            <br />
+            <br /> Contamos con experiencia en el área de derecho penal, civil, familiar,
+            laboral y administrativo, por lo que podremos asistirte de la mejor forma
+            posible en todas las fases del procedimiento.
+            <br />
+            <br /> También te asesoraremos sobre cualquier tema que te genere dudas o
+            desconocimiento.
+          </p>
+        </StyledDiv>
+        <StyledDiv display="flex" margin="1rem 0" gap="1rem">
+          <p>Valores</p>
+          <Img src={despacho} alt="despacho" width="30rem" />
+        </StyledDiv>
+      </StyledDiv>
+    </StyledDiv>
+  );
 };
--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -1,8 +1,39 @@
+import { NavLink } from 'react-router-dom';
+
+import despacho from '../../public/despachoAbogados.jpg';
+import { Img } from '../components/Img';
+import { StyledDiv } from '../components/StyledDiv';
+
 export const Principal = () => {
   return (
-    <h1>
-      Huye de los abogados deshumanizados, nosotros te daremos una atención cercana,
-      personalizada y sujeta a tus necesidades.
-    </h1>
+    <StyledDiv display="flex" flexdir="column" justify="center" padding="2rem 5%">
+      <h1
+        style={{ 'text-align': 'center', 'font-style': 'italic', 'font-weight': '600' }}
+      >
+        Huye de los abogados deshumanizados, nosotros te daremos una atención cercana,
+        personalizada y sujeta a tus necesidades.
+      </h1>
+      <StyledDiv display="flex" margin="2rem 0" gap="1rem">
+        <Img src={despacho} alt="despacho" width="30rem" height="20rem" />
+        <StyledDiv>
+          <p>
+            Ódena es un despacho de abogados situado en Madrid y Toledo, donde podremos
+            ayudarte con aquellas cuestiones jurídicas o procedimientos a los que te
+            enfrentes en materia civil, penal, laboral y administrativa.
+            <br />
+            <br /> Trataremos de que tus problemas se conviertan en una carga más ligera.
+            <br />
+            <br /> Te comprendemos y apoyamos tu situación.
+          </p>
+          <h3>PRIMERA CONSULTA TELEMÁTICA GRATUITA</h3>
+          <h3>
+            Para más información, pincha en{' '}
+            <NavLink to="contacto" className="navlink">
+              CONTACTO
+            </NavLink>
+          </h3>
+        </StyledDiv>
+      </StyledDiv>
+    </StyledDiv>
   );
 };
