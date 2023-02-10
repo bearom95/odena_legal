8875d5e251038235c089222844cd45bd0631eb6d
Laura <112576133+Lauris21@users.noreply.github.com>
2023-02-10 11:52:38 +0100

    Fix bug NavBar (#5)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,22 +3,22 @@ import { NavLink } from 'react-router-dom';
 
 import { NavbarWrapper } from './NavBarWrapper';
 
-function NavBar({ open }) {
+function NavBar({ open, handleClick }) {
   return (
     <NavbarWrapper open={open}>
-      <NavLink to="" className="navlink">
+      <NavLink to="" className="navlink" open={open} onClick={handleClick}>
         PRINCIPAL
       </NavLink>
-      <NavLink to="despacho" className="navlink">
+      <NavLink to="despacho" className="navlink" onClick={handleClick}>
         EL DESPACHO
       </NavLink>
-      <NavLink to="nosotros" className="navlink">
+      <NavLink to="nosotros" className="navlink" onClick={handleClick}>
         NOSOTROS
       </NavLink>
-      <NavLink to="blog" className="navlink">
+      <NavLink to="blog" className="navlink" onClick={handleClick}>
         BLOG
       </NavLink>
-      <NavLink to="contacto" className="navlink">
+      <NavLink to="contacto" className="navlink" onClick={handleClick}>
         CONTACTO
       </NavLink>
     </NavbarWrapper>
